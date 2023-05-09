e6bff2de9772769f50570fc5c4bc5d282b6798ca
RobertaKod <roberta00110110@icloud.com>
2023-05-09 21:16:44 +0200

    Initial Commit

---
+++ pinchZoom/pinchZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  pinchZoomApp.swift
+//  pinchZoom
+//
+//  Created by Roberta Marino on 09/05/23.
+//
+
+import SwiftUI
+
+@main
+struct pinchZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
